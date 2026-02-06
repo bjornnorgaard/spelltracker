ae9298bb4550abf923343c2e7dcd93610811ac80
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2026-02-07 00:54:23 +0100

    refactor: streamline character list and clean up formatting
    - Simplified character list by removing conditional logic around empty character states.
    - Adjusted `SectionHeader` and `PageHeader` components for consistent formatting.
    - Enhanced readability by cleaning up spacing and indentation.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,32 +5,23 @@
 </script>
 
 <div class="space-y-4">
-    <PageHeader title="Spell Tracker" subtitle="Track your spell slots and their spells." />
+    <PageHeader title="Spell Tracker" subtitle="Track your spell slots and their spells."/>
 
-    <SectionHeader title="Getting Started" subtitle="Follow these steps to get started with Spell Tracker." />
+    <SectionHeader title="Getting Started" subtitle="Follow these steps to get started with Spell Tracker."/>
     <ol class="list-decimal list-inside">
         <li>Create a character.</li>
         <li>Add spell slots.</li>
         <li>Import spells.</li>
     </ol>
 
-    <SectionHeader title="Your Characters" subtitle="Here are your characters." />
-    {#if app.current.characters.length}
-        <ul>
-            {#each app.current.characters as c}
-                <li class="list-inside list-disc">
-                    <a href={`/characters/${c.id}`} class="anchor">{c.name}, {c.class}, level {c.level}</a>
-                </li>
-            {/each}
-        </ul>
-    {:else}
-        <a href="/characters/new">
-            <div class="card p-4 preset-filled-warning-500">
-                <strong>You don't have any characters</strong>
-                <p class=""> Click this box to create a new character.</p>
-            </div>
-        </a>
-    {/if}
+    <SectionHeader title="Your Characters" subtitle="Here are your characters."/>
+    <ul>
+        {#each app.current.characters as c}
+            <li class="list-inside list-disc">
+                <a href={`/characters/${c.id}`} class="anchor">{c.name}, {c.class}, level {c.level}</a>
+            </li>
+        {/each}
+    </ul>
 
     <a href="/characters/new" class="btn w-full preset-filled-primary-500">Create New Character</a>
 </div>

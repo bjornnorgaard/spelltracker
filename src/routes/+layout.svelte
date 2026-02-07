5b7892ff80d27e419e7c88cb446c7ad3edfae686
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2026-02-07 13:36:01 +0100

    feat: enhance footer layout and integrate character links
    - Redesigned footer with updated styling and additional sections.
    - Added character links dynamically based on current app state.
    - Included navigation links and app description for improved usability.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import "./layout.css";
+    import {app} from "$lib/stores/app.svelte";
 
     let {children} = $props();
 </script>
@@ -8,8 +9,35 @@
     {@render children()}
 </main>
 
-<footer>
-    <div class="flex justify-center items-center py-16 preset-filled-surface-200-800">
-        <p class="text-sm">Spell Tracker By Bear</p>
+<footer class="p-8 mt-16 preset-filled-primary-50-950 space-y-4">
+    <div class="space-y-1">
+        <p>Spelltracker <span class="opacity-50">by Bear</span></p>
+        <p class="text-xs">A small and simple app to track spell list and slots.</p>
+    </div>
+
+    <div class="flex gap-4 justify-between my-8">
+        <div class="space-y-2 ">
+            <p class="uppercase opacity-75 text-xs tracking-widest">Links</p>
+            <ul class="space-y-1">
+                <li><a class="anchor" href="/">Home</a></li>
+                <li><a class="anchor" href="/spells">Spells</a></li>
+            </ul>
+        </div>
+        <div class="space-y-2">
+            <p class="uppercase opacity-75 tracking-widest text-xs">Characters</p>
+            <ul class="space-y-1">
+                {#if (app.current.characters ?? []).length > 0}
+                    {#each app.current.characters ?? [] as c}
+                        <li><a class="anchor" href={`/characters/${c.id}`}>{c.name}</a></li>
+                    {/each}
+                {:else}
+                    <li class="text-surface-500-200">No characters yet</li>
+                {/if}
+            </ul>
+        </div>
+    </div>
+    <div class="flex items-center justify-between text-xs opacity-50">
+        <span>Spell Tracker by Bear</span>
+        <span>Made for quick table use</span>
     </div>
 </footer>

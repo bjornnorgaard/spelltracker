a1337f2a4a642ece20788e0cbdee0e39f0ca3cbb
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2026-02-13 13:26:39 +0100

    refactor: add type annotations to LocalStorage for spells and characters in stores module

--- src/lib/stores/stores.ts
+++ src/lib/stores/stores.ts
@@ -1,5 +1,7 @@
 import {LocalStorage} from "$lib/utils/storage.svelte";
+import type {Spell} from "$lib/types/spell";
+import type {Character} from "$lib/types/character";
 
 const prefix = "spelltracker"
-export const spells = new LocalStorage(prefix + ":spells", []);
-export const characters = new LocalStorage(prefix + ":characters", []);
+export const spells = new LocalStorage<Spell[]>(prefix + ":spells", []);
+export const characters = new LocalStorage<Character[]>(prefix + ":characters", []);

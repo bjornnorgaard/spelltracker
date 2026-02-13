2420dccd499ca6d3cf0913384b2b6b50ae78562b
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2026-02-13 13:40:08 +0100

    refactor: improve SectionHeader layout with updated structure, alignment, and styling adjustments

--- src/lib/components/SectionHeader.svelte
+++ src/lib/components/SectionHeader.svelte
@@ -2,5 +2,11 @@
     const {title, subtitle} = $props();
 </script>
 
-<h1 class="preset-typo-title mb-0">{title}</h1>
-<p class="preset-typo-caption leading-4">{subtitle}</p>
+<div class="flex flex-col">
+    <div class="flex gap-2 items-center">
+        <div class="h-1 w-4 card preset-filled-surface-500"></div>
+        <h1 class="preset-typo-title w-fit mb-0 ">{title}</h1>
+        <div class="h-1 grow card preset-filled-surface-500"></div>
+    </div>
+    <p class="text-surface-600-400 text-sm">{subtitle}</p>
+</div>

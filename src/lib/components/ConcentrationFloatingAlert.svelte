6c46fba54958efab7209b4c648b875d3ef48df19
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2026-02-13 11:53:47 +0100

    refactor: streamline ConcentrationFloatingAlert layout, adjust spacing, and simplify damage description

--- src/lib/components/ConcentrationFloatingAlert.svelte
+++ src/lib/components/ConcentrationFloatingAlert.svelte
@@ -13,22 +13,17 @@
 
 {#if spell}
     <div class="fixed preset-filled-secondary-500 px-4 py-2 border-b-4 border-b-secondary-300-700 shadow-2xl top-0 left-0 right-0 z-50" transition:fly={{ y: -100, duration: 300 }}>
-
         <div class="flex justify-between opacity-75 items-center">
             <p class="text-shadow-surface-200-800 font-bold uppercase tracking-widest">Currently Concentrating</p>
             <button onclick={ondrop}>
                 <X/>
             </button>
         </div>
-
         <p class="preset-typo-headline flex gap-2 items-center">
             <Brain class="animate-pulse"/>
             {spell.name}
         </p>
-
-        <p class="font-bold text-sm">{spell.duration}</p>
-
-        <p class="text-xs opacity-75">On Damage: Constitution save DC is half of the damage (rounded down), minimum of 10.</p>
-
+        <p class="font-bold text-sm mt-2">{spell.duration}</p>
+        <p class="text-xs opacity-75">Damage: Constitution save DC is half of the damage (rounded down), minimum of 10.</p>
     </div>
 {/if}
